53a5300c0349e687382a2a9a262ae70039d30d7e
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-09-30 21:11:35 -0500

    worked on some styling issues

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,4 +1,9 @@
-angular.module('app', ['ngRoute'])
+angular.module('app', ['ngRoute', 'angular-momentjs'])
+.config(function($momentProvider){
+	$momentProvider
+	.asyncLoading(false)
+	.scriptUrl('moment/min/moment.min.js');
+})
 .config(function($routeProvider){
 	$routeProvider
 	.when('/', {
